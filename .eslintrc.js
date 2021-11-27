81189fa3e3dd27d31aa7ca52fbfeedd9c941c4a2
kazu1496 <kazumasa1496@gmail.com>
2021-11-28 00:32:49 +0900

    initial commit🚀

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+/**
+ * @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config}
+ */
+eslintConfig = {
+  extends: ['next/core-web-vitals', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['simple-import-sort', 'unused-imports'],
+  rules: {
+    semi: ['error', 'always'],
+    'semi-spacing': ['error', { after: true, before: false }],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-unused-vars': 0,
+    'unused-imports/no-unused-imports-ts': 'warn',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
+
+module.exports = eslintConfig;
