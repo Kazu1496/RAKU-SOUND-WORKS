c227c1eb8498a40072745b0b00f42b9bd615084d
kazu1496 <kazumasa1496@gmail.com>
2021-11-29 11:07:00 +0900

    Fix: 画像ホバーアニメーション修正

--- components/templates/Works/style.ts
+++ components/templates/Works/style.ts
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { rgba } from 'polished';
 
 import { color, font, spacing } from '@/theme';
 import { mq } from '@/theme/mediaQuery';
@@ -28,10 +27,55 @@ export const Item = styled.li(() =>
   mq({
     backgroundColor: color.white,
     position: 'relative',
+    display: 'inline-block',
     cursor: 'pointer',
     height: 'calc(320px * 0.5625)',
-    '&:hover div': {
+    overflow: 'hidden',
+    textAlign: 'left',
+    background: '#000',
+    '& *, &:before, &:after': {
+      transition: 'all 0.4s ease',
+    },
+    '& img': {
+      maxWidth: '100%',
+      backfaceVisibility: 'hidden',
+      verticalAlign: 'top',
+    },
+    '&:before, &:after': {
+      position: 'absolute',
+      top: '20px',
+      right: '20px',
+      content: '""',
+      backgroundColor: '#fff',
+      zIndex: 1,
+      opacity: 0,
+    },
+    '&:before': {
+      width: 0,
+      height: '1px',
+    },
+    '&:after': {
+      height: 0,
+      width: '1px',
+    },
+    '&:hover img': {
+      zoom: 1,
+      filter: 'alpha(opacity=20)',
+      opacity: 0.2,
+    },
+    '&:hover:before, &:hover:after': {
       opacity: 1,
+      transitionDelay: '0.25s',
+    },
+    '&:hover:before': {
+      width: '40px',
+    },
+    '&:hover:after': {
+      height: '40px',
+    },
+    '&:hover p': {
+      opacity: 1,
+      transitionDelay: '0.35s',
     },
   }),
 );
@@ -39,26 +83,27 @@ export const Item = styled.li(() =>
 export const WorkAbout = styled.div(() =>
   mq({
     position: 'absolute',
-    top: 0,
     left: 0,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-    backgroundColor: rgba(0, 0, 0, 0.8),
-    opacity: 0,
-    transition: '.3s ease-in-out',
+    bottom: 0,
+    padding: '15px 20px',
   }),
 );
 
 export const Title = styled.p(() =>
   mq({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
     color: color.white,
-    fontSize: font.xxl,
+    fontSize: font.m,
     fontWeight: 'bold',
-    marginBottom: spacing.m,
+    opacity: 0,
+    margin: 0,
+    '& > span': {
+      fontSize: font.xs,
+      fontWeight: 'normal',
+      marginTop: spacing.xs,
+    },
   }),
 );
 
