bd874229d919e3efe3f9734534863f7e9eb05447
kazu1496 <kazumasa1496@gmail.com>
2021-11-30 20:57:49 +0900

    Fix: strictModeをfalseに変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: ['images.microcms-assets.io', 'img.youtube.com'],
     formats: ['image/avif', 'image/webp'],
