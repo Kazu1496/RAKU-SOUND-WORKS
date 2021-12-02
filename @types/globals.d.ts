b38aeff2310bb4c2a67fab2e150c4de83d228b5b
kazu1496 <kazumasa1496@gmail.com>
2021-12-03 00:02:02 +0900

    Add: メール送信機能の実装

--- @types/globals.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable unused-imports/no-unused-vars */
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly MICROCMS_SERVICE_ID: string;
-    readonly MICROCMS_API_KEY: string;
-  }
-}
